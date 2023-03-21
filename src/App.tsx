ce54780b63066f62863092458a1dead38ce6a20a
Brandon Fulljames <bfullj@gmail.com>
2023-03-21 20:46:54 +0900

    Show golf hole

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 
+//<img src={logo} className="App-logo" alt="logo" />
 function App() {
   return (
+    <React.Fragment>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="course/chiba-shimin/holes/1.png" alt="hole" />
       </header>
     </div>
+    </React.Fragment>
   );
 }
 
