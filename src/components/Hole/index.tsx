81d793842d035cc135edddaa2f8affcf3a8ae981
Brandon Fulljames <bfullj@gmail.com>
2023-03-22 23:17:52 +0900

    Show coords over image for debugging

--- src/components/Hole/index.tsx
+++ src/components/Hole/index.tsx
@@ -5,19 +5,30 @@ export interface HoleProps {
 }
 
 const Hole = (props: HoleProps) => {
-  const [mouseCoords, setMouseCoords] = useState({ x: 0, y: 0 });
+  const [mouseCoords, setMouseCoords] = useState({
+    xFractional: 0,
+    yFractional: 0,
+  });
 
   const handleMouseMove = (e: React.MouseEvent<HTMLImageElement>) => {
+    const x = e.pageX - e.currentTarget.offsetLeft;
+    const y = e.pageY - e.currentTarget.offsetTop;
+    const width = e.currentTarget.width;
+    const height = e.currentTarget.height;
+
     setMouseCoords({
-      x: e.pageX - e.currentTarget.offsetLeft,
-      y: e.pageY - e.currentTarget.offsetTop,
+      xFractional: x / width,
+      yFractional: y / height,
     });
   };
+
+  const xPercent = (mouseCoords.xFractional * 100).toFixed(1);
+  const yPercent = (mouseCoords.yFractional * 100).toFixed(1);
 
   return (
     <React.Fragment>
       <div>
-        X: {mouseCoords.x}, Y: {mouseCoords.y}
+        X: {xPercent}%, Y: {yPercent}%
       </div>
       <img
         src={props.imgSrc}
