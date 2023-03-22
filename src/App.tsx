74234337b4a92597b231fc1f6dd72f7899401ed8
Brandon Fulljames <bfullj@gmail.com>
2023-03-22 23:41:48 +0900

    Use ts baseUrl to clean up (#32)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 //import logo from "./logo.svg";
 import './App.css';
-import Hole from './components/Hole';
+import Hole from 'components/Hole';
 
 //<img src={logo} className="App-logo" alt="logo" />
 function App() {
