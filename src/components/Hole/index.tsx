e25e7079207b2b9e271cfd1be20caea7a81f61ee
Brandon Fulljames <bfullj@gmail.com>
2023-03-22 22:55:59 +0900

    Track mouse over image

--- src/components/Hole/index.tsx
+++ src/components/Hole/index.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export interface HoleProps {
   imgSrc: string;
 }
 
 const Hole = (props: HoleProps) => {
+  const [mouseCoords, setMouseCoords] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLImageElement>) => {
+    setMouseCoords({
+      x: e.pageX - e.currentTarget.offsetLeft,
+      y: e.pageY - e.currentTarget.offsetTop,
+    });
+  };
+
   return (
     <React.Fragment>
-      <img src={props.imgSrc} alt={props.imgSrc} />
+      <div>
+        X: {mouseCoords.x}, Y: {mouseCoords.y}
+      </div>
+      <img
+        src={props.imgSrc}
+        alt={props.imgSrc}
+        onMouseMove={handleMouseMove}
+      />
     </React.Fragment>
   );
 };
