339be819b24a219be322fd05dfebb116195c6c7a
Brandon Fulljames <bfullj@gmail.com>
2023-04-05 21:47:21 +0900

    Track shots individually per hole and allow reset

--- src/components/Round/index.tsx
+++ src/components/Round/index.tsx
@@ -1,5 +1,6 @@
 import Hole from 'components/Hole';
-import React from 'react';
+import React, { useState } from 'react';
+import './Round.css';
 
 export interface RoundProps {
   bag: Shot[];
@@ -7,14 +8,58 @@ export interface RoundProps {
 }
 
 const Round = ({ bag, course }: RoundProps) => {
-  const currentHole = course.holes[1];
+  const [currentHoleNumber, setCurrentHoleNumber] = useState(1);
+
+  const [shotHistoryByHole, setShotHistoryByHole] = useState(
+    Array.from({ length: course.holes.length }, (): ShotHistory[] => [])
+  );
+
+  const currentHoleData = course.holes[currentHoleNumber - 1];
+
+  const goBack = () => {
+    if (currentHoleNumber === 1) {
+      setCurrentHoleNumber(course.holes.length);
+    } else {
+      setCurrentHoleNumber(currentHoleNumber - 1);
+    }
+  };
+
+  const goForward = () => {
+    if (currentHoleNumber === course.holes.length) {
+      setCurrentHoleNumber(1);
+    } else {
+      setCurrentHoleNumber(currentHoleNumber + 1);
+    }
+  };
+
+  const resetHole = () => {
+    const copied = [...shotHistoryByHole];
+    copied[currentHoleNumber - 1] = [];
+    setShotHistoryByHole(copied);
+  };
+
+  const takeShot = (shot: ShotHistory) => {
+    const copied = [...shotHistoryByHole];
+    copied[currentHoleNumber - 1].push(shot);
+    setShotHistoryByHole(copied);
+  };
 
   return (
     <React.Fragment>
       <h2>
-        {course.name} #{currentHole.holeNumber} (Par {currentHole.par})
+        {course.name} #{currentHoleNumber} (Par {currentHoleData.par})
       </h2>
-      <Hole bag={bag} data={currentHole} />
+      <div className="RoundToolbar">
+        <div onMouseDown={goBack}>{'<'}</div>
+        <div onMouseDown={resetHole}>Reset</div>
+        <div onMouseDown={goForward}>{'>'}</div>
+      </div>
+      <Hole
+        bag={bag}
+        data={currentHoleData}
+        shotsTaken={shotHistoryByHole[currentHoleNumber - 1]}
+        takeShot={takeShot}
+      />
     </React.Fragment>
   );
 };
