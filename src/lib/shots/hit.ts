96d22170fff99d5636dd18087442a0706ba167d7
Brandon Fulljames <bfullj@gmail.com>
2023-04-08 17:17:37 +0900

    Putting (#89)
    * Putt out when on green
    
    * Only show yardages when hole isn't complete yet
    
    * Fix hole #3 terrain reference

---
+++ src/lib/shots/hit.ts
@@ -0,0 +1,59 @@
+import { boundDegrees, degreesToRadians } from 'lib/math';
+
+// 0 degrees is straight east, increasing clockwise
+export function hitShot(
+  shot: Shot,
+  source: Coords,
+  initialDirectionDegrees: number
+): ShotResult {
+  if (shot.potentialOutcomes.length === 0) {
+    throw new Error('shot has no outcomes');
+  }
+
+  const outcome =
+    shot.potentialOutcomes[
+      Math.floor(Math.random() * shot.potentialOutcomes.length)
+    ];
+
+  const startSpreadDegrees =
+    outcome.startDegreesRightmost - outcome.startDegreesLeftmost;
+  const startDegrees = boundDegrees(
+    Math.random() * startSpreadDegrees +
+      outcome.startDegreesLeftmost +
+      initialDirectionDegrees
+  );
+
+  const sidespinSpreadDegrees =
+    outcome.sidespinDegreeRightmost - outcome.sidespinDegreeLeftmost;
+  const endDegrees = boundDegrees(
+    Math.random() * sidespinSpreadDegrees +
+      outcome.sidespinDegreeLeftmost +
+      startDegrees
+  );
+
+  // For now, we cheat a little in figuring out an exact path in favor of getting
+  // something that's close enough and easy to draw later.
+  const endRadians = degreesToRadians(endDegrees);
+  const distanceModifier = Math.cos(
+    degreesToRadians(startDegrees - endDegrees) * 2
+  );
+  const totalDistanceSpread = outcome.carryYardsMax - outcome.carryYardsMin;
+  const totalDistanceYards =
+    (Math.random() * totalDistanceSpread + outcome.carryYardsMin) *
+    distanceModifier;
+
+  const xDistanceYards = Math.cos(endRadians) * totalDistanceYards;
+  const yDistanceYards = Math.sin(endRadians) * totalDistanceYards;
+
+  const landingSpot = {
+    xYards: source.xYards + xDistanceYards,
+    yYards: source.yYards + yDistanceYards,
+  };
+
+  return {
+    source,
+    landingSpot,
+    startDegrees,
+    endDegrees,
+  };
+}
